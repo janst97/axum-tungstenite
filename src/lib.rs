9496c5da96e377cc97fc10068ca2e2b21e6dd906
Jan Stoyke <michael-jan.stoyke@tu-ilmenau.de>
2024-09-06 08:41:27 +0200

    Update dependencies to recent versions

--- src/lib.rs
+++ src/lib.rs
@@ -94,7 +94,7 @@
     missing_debug_implementations,
     missing_docs
 )]
-#![deny(unreachable_pub, private_in_public)]
+#![deny(unreachable_pub, private_interfaces, private_bounds)]
 #![allow(elided_lifetimes_in_paths, clippy::type_complexity)]
 #![forbid(unsafe_code)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg))]
@@ -117,6 +117,7 @@ use http::{
     Method, StatusCode,
 };
 use hyper::upgrade::{OnUpgrade, Upgraded};
+use hyper_util::rt::TokioIo;
 use sha1::{Digest, Sha1};
 use std::{
     borrow::Cow,
@@ -255,7 +256,7 @@ impl<C> WebSocketUpgrade<C> {
 
         tokio::spawn(async move {
             let upgraded = match on_upgrade.await {
-                Ok(upgraded) => upgraded,
+                Ok(upgraded) => TokioIo::new(upgraded),
                 Err(err) => {
                     on_failed_upgrade.call(err);
                     return;
@@ -399,13 +400,13 @@ fn header_contains(req: &Parts, key: HeaderName, value: &'static str) -> bool {
 /// A stream of WebSocket messages.
 #[derive(Debug)]
 pub struct WebSocket {
-    inner: WebSocketStream<Upgraded>,
+    inner: WebSocketStream<TokioIo<Upgraded>>,
     protocol: Option<HeaderValue>,
 }
 
 impl WebSocket {
     /// Consume `self` and get the inner [`tokio_tungstenite::WebSocketStream`].
-    pub fn into_inner(self) -> WebSocketStream<Upgraded> {
+    pub fn into_inner(self) -> WebSocketStream<TokioIo<Upgraded>> {
         self.inner
     }
 
